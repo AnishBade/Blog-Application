2de1551484115a8624bfad387cba70770d53644c
AnishBade <badeanish6045@gmail.com>
2022-09-22 15:55:56 +0545

    tagging and some custom templates added

--- blog/views.py
+++ blog/views.py
@@ -10,9 +10,15 @@ from django.conf import settings
 from django.core.mail import send_mail
 from .models import Post, Comment 
 from .forms import EmailPostForm, CommentForm
+from taggit.models import Tag
+from django.db.models import Count
 
-def post_list(request):
+def post_list(request,tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug) 
+        object_list = object_list.filter(tags__in=[tag])
     paginator = Paginator(object_list, 3) # 3 posts in each page
     page = request.GET.get('page')
     try:
@@ -23,10 +29,15 @@ def post_list(request):
     except EmptyPage:
         # If page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
+    
     return render(request,
                  'blog/post/list.html',
                  {'page': page,
-                  'posts': posts})
+                  'posts': posts,
+                  'tag': tag})
+
+ 
+
 
 
 def post_detail(request, year, month, day, post):
@@ -49,9 +60,13 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post,'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form})
+                  {'post': post,'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form,'similar_posts': similar_posts})
 
 
 class PostListView(ListView):
@@ -59,6 +74,7 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
+
 
 
 
